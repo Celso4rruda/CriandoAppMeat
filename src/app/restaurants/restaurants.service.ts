c283cf61e104d0a98febeefaaa141ff29295379e
Maverick <celso4rruda@gmail.com>
2021-05-26 17:05:18 -0300

    menu e reviews re-implementados dinamicamente

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -8,6 +8,7 @@ import { Restaurant } from "./restaurant/restaurant.model";
 import {MEAT_API} from '../app.api'
 import { Observable } from 'rxjs/Observable';
 import { ErrorHandler } from '../error-handler'
+import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model';
 
 @Injectable()
 export class RestaurantsService{
@@ -19,5 +20,19 @@ export class RestaurantsService{
 
         return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json()).catch(ErrorHandler.handleError)
 
+    }
+
+    restaurantById(id: string): Observable<Restaurant>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}`).map(response => response.json()).catch(ErrorHandler.handleError)
+    }
+
+    reviewsOfRestaurant(id: string): Observable<any>{
+
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`).map(response => response.json()).catch(ErrorHandler.handleError)
+
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`).map(response => response.json()).catch(ErrorHandler.handleError)
     }
 }
